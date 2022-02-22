5be684a9c9014fd5768c23a1f0a2cd0e96580d58
unknown <hassan.raza@kryptomind.com>
2022-02-22 17:11:52 +0500

    cleanup

--- src/panel.config.js
+++ src/panel.config.js
@@ -2,12 +2,12 @@ const panelConfig = {
   /**
    Production
    */
-  // API_URL: "https://main.blockmerchants.app",
+  API_URL: "https://api-icovest.cqrvault.org",
   // API_URL: "http://f48e-101-53-234-165.ngrok.io",
   /**
    Development
    */
-  API_URL: "http://localhost:8080",
+  // API_URL: "http://localhost:8080",
 };
 
 export default panelConfig;
