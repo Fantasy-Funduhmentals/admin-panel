563836fafdb9f80413584776296289291e33ccf0
Xaad Ali <saad.rafique@kryptomind.com>
2022-06-17 18:56:20 +0500

    ui

--- src/components/sub-admin/sub-admin.tsx
+++ src/components/sub-admin/sub-admin.tsx
@@ -31,108 +31,108 @@ interface Props extends CardProps {
   searchQuery?: string;
 }
 
-// export default function AlertDialog({ data }) {
-//   const [open, setOpen] = useState(false);
-//   const [loading, setLoading] = useState(false);
-//   const [statusData, setStatusData] = useState(null);
-//   const [checked, setChecked] = useState(data?.block);
-//   const [some, setSome] = useState(false);
-//   const dispatch = useAppDispatch();
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
+export default function AlertDialog({ data }) {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [statusData, setStatusData] = useState(null);
+  const [checked, setChecked] = useState(data?.block);
+  const [some, setSome] = useState(false);
+  const dispatch = useAppDispatch();
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-//   const handleClose = () => {
-//     setOpen(false);
-//   };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-//   function capitalizeFirstLetter(str: string) {
-//     return str[0].toUpperCase() + str.slice(1);
-//   }
+  function capitalizeFirstLetter(str: string) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
 
-//   const handleUserBlock = async (
-//     event: React.ChangeEvent<HTMLInputElement>
-//   ) => {
-//     try {
-//       setChecked(event.target.checked == true ? 1 : 0);
-//       setLoading(true);
-//       await HTTP_CLIENT.get(`/auth/user-block/${data?.id}`);
-//       setSome(!some);
-//       setLoading(false);
-//       handleClose();
-//     } catch (err) {
-//       const error = getNormalizedError(err);
-//       setStatusData({
-//         type: "error",
-//         message: error,
-//       });
-//       setLoading(false);
-//     }
-//   };
-//   useEffect(() => {
-//     getAdminUsers();
-//   }, [some]);
+  const handleUserBlock = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    try {
+      setChecked(event.target.checked == true ? 1 : 0);
+      setLoading(true);
+      await HTTP_CLIENT.get(`/auth/user-block/${data?.id}`);
+      setSome(!some);
+      setLoading(false);
+      handleClose();
+    } catch (err) {
+      const error = getNormalizedError(err);
+      setStatusData({
+        type: "error",
+        message: error,
+      });
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    getAdminUsers();
+  }, [some]);
 
-//   const getAdminUsers = async () => {
-//     try {
-//       // const AdminUser = await getAdminUserData();
-//       // dispatch(saveAdminUser(AdminUser.data));
-//     } catch (err) {
-//       const error = getNormalizedError(err);
-//       setStatusData({
-//         type: "error",
-//         message: error,
-//       });
-//     }
-//   };
+  const getAdminUsers = async () => {
+    try {
+      // const AdminUser = await getAdminUserData();
+      // dispatch(saveAdminUser(AdminUser.data));
+    } catch (err) {
+      const error = getNormalizedError(err);
+      setStatusData({
+        type: "error",
+        message: error,
+      });
+    }
+  };
 
-//   return (
-//     <div>
-//       <SeverityPill
-//         color={(data?.block === 0 && "error") || "success"}
-//         onClick={handleClickOpen}
-//         style={{ cursor: "pointer" }}
-//       >
-//         {data?.block === 0 ? "Block " : "Unblock "}
-//       </SeverityPill>
+  return (
+    <div>
+      <SeverityPill
+        color={(data?.block === 0 && "error") || "success"}
+        onClick={handleClickOpen}
+        style={{ cursor: "pointer" }}
+      >
+        {data?.block === 0 ? "Block " : "Unblock "}
+      </SeverityPill>
 
-//       <StatusModal
-//         statusData={statusData}
-//         onClose={() => setStatusData(null)}
-//       />
-//       <Dialog
-//         open={open}
-//         onClose={handleClose}
-//         aria-labelledby="alert-dialog-title"
-//         aria-describedby="alert-dialog-description"
-//       >
-//         <DialogContent>
-//           {data?.block == 1 ? (
-//             <DialogContentText id="alert-dialog-description">
-//               Are you sure want to Block
-//             </DialogContentText>
-//           ) : (
-//             <DialogContentText id="alert-dialog-description">
-//               Are you sure want to Unblock
-//             </DialogContentText>
-//           )}
-//         </DialogContent>
-//         <DialogActions>
-//           <Button onClick={handleClose}>Cancel</Button>
-//           {loading ? (
-//             <Button>Loading...</Button>
-//           ) : (
-//             <Button onClick={handleUserBlock}>Confirm</Button>
-//           )}
-//         </DialogActions>
-//       </Dialog>
-//       <StatusModal
-//         statusData={statusData}
-//         onClose={() => setStatusData(null)}
-//       />
-//     </div>
-//   );
-// }
+      <StatusModal
+        statusData={statusData}
+        onClose={() => setStatusData(null)}
+      />
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          {data?.block == 1 ? (
+            <DialogContentText id="alert-dialog-description">
+              Are you sure want to Block
+            </DialogContentText>
+          ) : (
+            <DialogContentText id="alert-dialog-description">
+              Are you sure want to Unblock
+            </DialogContentText>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          {loading ? (
+            <Button>Loading...</Button>
+          ) : (
+            <Button onClick={handleUserBlock}>Confirm</Button>
+          )}
+        </DialogActions>
+      </Dialog>
+      <StatusModal
+        statusData={statusData}
+        onClose={() => setStatusData(null)}
+      />
+    </div>
+  );
+}
 export const AdminsList = (props: Props) => {
   const { data, searchQuery } = props;
   const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
@@ -185,6 +185,7 @@ export const AdminsList = (props: Props) => {
                   <TableCell>Email</TableCell>
                   <TableCell>roles</TableCell>
                   <TableCell>Permissions</TableCell>
+                  <TableCell>Block/Unblock</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -201,6 +202,9 @@ export const AdminsList = (props: Props) => {
                     <TableCell>
                       {customer?.adminPermissions?.length > 0 &&
                         customer.adminPermissions}
+                    </TableCell>
+                    <TableCell>
+                      <AlertDialog data={customer} />
                     </TableCell>
                   </TableRow>
                 ))}
