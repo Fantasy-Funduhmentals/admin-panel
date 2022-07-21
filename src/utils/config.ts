e2f206a3355241bb94e649b19a8666771b97a262
bilal0075 <95081866+bilal0075@users.noreply.github.com>
2022-07-21 19:25:46 +0500

    staging link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,16 +1,16 @@
 const testnetConfig = {
   Base_URL: "https://staging-api.cqrvault.org/",
 };
-// const mainnetConfig = {
-//   Base_URL: "https://staging-api.cqrvault.org/",
-// };
+const mainnetConfig = {
+  Base_URL: "https://staging-api.cqrvault.org/",
+};
 /**** */
 // const mainnetConfig = {
 //   Base_URL: "https://api-icovest.cqrvault.org ",
 // };
-const mainnetConfig = {
-  Base_URL: "https://api.cqrvault.org/",
-};
+// const mainnetConfig = {
+//   Base_URL: "https://api.cqrvault.org/",
+// };
 
 const defaultConfig = mainnetConfig;
 
