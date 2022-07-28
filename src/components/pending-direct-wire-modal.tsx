670e1438aaefb8ab572fb0e923a02bcb901d9190
bilal0075 <95081866+bilal0075@users.noreply.github.com>
2022-07-28 16:13:09 +0500

    changes

--- src/components/pending-direct-wire-modal.tsx
+++ src/components/pending-direct-wire-modal.tsx
@@ -333,7 +333,7 @@ const PendingDirectWireModal = (props: Props) => {
                       variant="h4"
                       sx={{ pb: 3, textAlign: "center" }}
                     >
-                      Remittance Address
+                      Banking Details
                     </Typography>
                     <Card sx={{ display: "flex", width: "100%" }}>
                       <Box
