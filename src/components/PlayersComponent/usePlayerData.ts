083e756ac946ae0aa3b3d1ce3fc81fdb1c2459bd
bilal0075 <muhammad.bilal@kryptomind.com>
2022-11-22 19:02:41 +0500

    players api

--- src/components/PlayersComponent/usePlayerData.ts
+++ src/components/PlayersComponent/usePlayerData.ts
@@ -1,8 +1,11 @@
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import { useMemo, useState } from "react";
 import { handleBlockSubAdmin } from "../../services/userService";
 import { RootState } from "../../store";
 import { useAppSelector } from "../../store/hooks";
 import { getNormalizedError } from "../../utils/helpers";
+import { updatePlayerValue } from "../../services/playerService";
 
 const usePlayerData = (
   searchQuery,
@@ -19,9 +22,18 @@ const usePlayerData = (
   const [reason, setReason] = useState(null);
   const [statusData, setStatusData] = useState(null);
   const [loading, setloading] = useState(false);
+  const [playerId, setPlayerId] = useState(null);
 
   const handleTextAreaChange = (e) => {
     setReason(e.target.value);
+  };
+  const handleEditPlayer = (data: any) => {
+    console.log(
+      "🚀 ~ file: usePlayerData.ts ~ line 27 ~ handleEditPlayer ~ data",
+      data
+    );
+    setPlayerId(data?._id);
+    setrejectShow(true);
   };
 
   const dataToDisplay = useMemo(() => {
@@ -36,75 +48,114 @@ const usePlayerData = (
     }
   }, [data, searchQuery]);
 
-  const handleBlockUser = (data) => {
-    if (data.isBlocked) {
-      setsignleUser(data);
-      handleSubmit(data);
-    } else {
-      setsignleUser(data);
-      setrejectShow(true);
-    }
-  };
+  // const handleBlockUser = (data) => {
+  //   if (data.isBlocked) {
+  //     setsignleUser(data);
+  //     handleSubmit(data);
+  //   } else {
+  //     setsignleUser(data);
+  //     setrejectShow(true);
+  //   }
+  // };
 
   const handleClose = () => {
     setrejectShow(false);
   };
-
-  const handleSubmit = async (data) => {
-    let params;
-    if (data?.isBlocked) {
-      params = {
-        userId: data?._id,
-        isblocked: !data?.isBlocked,
-      };
-    } else {
-      if (!reason) {
-        setStatusData({
-          type: "error",
-          message: "please enter reason first",
-        });
-        return;
-      }
-      params = {
-        userId: signleUser?._id,
-        isblocked: !signleUser?.isBlocked,
-        reasonToBlock: reason,
-      };
-    }
+  const formik = useFormik({
+    initialValues: {
+      value: "",
+    },
+    validationSchema: Yup.object({
+      value: Yup.number().positive().required(),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      handleSubmit(values, { resetForm });
+    },
+  });
+  const handleSubmit = async (values, { resetForm }) => {
+    let params = {
+      value: Number(values.value),
+    };
 
     try {
       setloading(true);
-      const response = await handleBlockSubAdmin(params);
+      const res = await updatePlayerValue(playerId, params);
       setStatusData({
         type: "success",
-        message: response?.data?.message,
+        message: res?.data?.message,
       });
-      setloading(false);
-      handleClose();
+      resetForm();
+      setrejectShow(false);
+
       RefreshAdminUsersData();
+      setloading(false);
     } catch (err) {
+      setloading(false);
       const error = getNormalizedError(err);
       setStatusData({
         type: "error",
         message: error,
       });
-      setloading(false);
     }
   };
+
+  // const handleSubmit = async (data) => {
+  //   let params;
+  //   if (data?.isBlocked) {
+  //     params = {
+  //       userId: data?._id,
+  //       isblocked: !data?.isBlocked,
+  //     };
+  //   } else {
+  //     if (!reason) {
+  //       setStatusData({
+  //         type: "error",
+  //         message: "please enter reason first",
+  //       });
+  //       return;
+  //     }
+  //     params = {
+  //       userId: signleUser?._id,
+  //       isblocked: !signleUser?.isBlocked,
+  //       reasonToBlock: reason,
+  //     };
+  //   }
+
+  //   try {
+  //     setloading(true);
+  //     const response = await handleBlockSubAdmin(params);
+  //     setStatusData({
+  //       type: "success",
+  //       message: response?.data?.message,
+  //     });
+  //     setloading(false);
+  //     handleClose();
+  //     RefreshAdminUsersData();
+  //   } catch (err) {
+  //     const error = getNormalizedError(err);
+  //     setStatusData({
+  //       type: "error",
+  //       message: error,
+  //     });
+  //     setloading(false);
+  //   }
+  // };
 
   return {
     dataToDisplay,
     selectedCustomerIds,
-    handleBlockUser,
-
+    // handleBlockUser,
+    handleEditPlayer,
     rejectShow,
     handleClose,
     loading,
     handleTextAreaChange,
-    handleSubmit,
+    // handleSubmit,
     statusData,
     setStatusData,
     subadmin,
+    formik,
+    // handlePlayerValue,
   };
 };
 
--- src/services/playerService.ts
+++ src/services/playerService.ts
@@ -3,4 +3,7 @@ import { HTTP_CLIENT } from "../utils/axiosClient";
 const handlePlayersData = async (page: any, limit: any) => {
   return await HTTP_CLIENT.get(`/players?page=${page}&limit=${limit}`);
 };
-export { handlePlayersData };
+const updatePlayerValue = async (playerId: any, params: any) => {
+  return await HTTP_CLIENT.put(`/players/${playerId}/update-value`, params);
+};
+export { handlePlayersData, updatePlayerValue };
