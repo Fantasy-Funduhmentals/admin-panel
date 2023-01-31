bb5231ccd08f7cfb2228f456677e1fd7913f478c
mehboobilya <mahboob.ilyas@kryptomind.com~>
2023-01-31 15:13:42 +0500

    position module

--- src/services/teamService.ts
+++ src/services/teamService.ts
@@ -3,4 +3,28 @@ import { HTTP_CLIENT } from "../utils/axiosClient";
 const handleTeamsData = async (page: any, limit: any) => {
   return await HTTP_CLIENT.get(`/teams?page=${page}&limit=${limit}`);
 };
-export { handleTeamsData };
+
+const handlePositionData = async () => {
+  return await HTTP_CLIENT.get("/position");
+};
+
+const postPsitionData = async (params: any) => {
+  return await HTTP_CLIENT.post(`/position`, params);
+};
+const updatePsitionData = async (params: any, id: string) => {
+  return await HTTP_CLIENT.put(`/position/${id}`, params);
+};
+const deletePsitionData = async (id: string) => {
+  return await HTTP_CLIENT.delete(`/position/${id}`);
+};
+const getPositionStatus = async (params: any) => {
+  return await HTTP_CLIENT.post("/position/status", params);
+};
+export {
+  handleTeamsData,
+  handlePositionData,
+  postPsitionData,
+  updatePsitionData,
+  deletePsitionData,
+  getPositionStatus,
+};
